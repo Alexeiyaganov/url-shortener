1eed07b70f51ff2fa25e2a39c7e08faa517d11a3
Alexeiyaganov <BTLS3@yandex.ru>
2022-01-30 19:49:49 +0300

    After moderations

--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS shorter;
-
-CREATE TABLE IF NOT EXISTS shorter
-(
-    id           SERIAL PRIMARY KEY,
-    hash         varchar(20) not null unique,
-    original_url varchar,
-    created_at   timestamp
-);
